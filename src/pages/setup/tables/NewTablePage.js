e64a5564a1d12da2d05c004945fcb4700acedfcb
Ead <ead.bunnamon@oivan.com>
2022-06-28 23:04:24 +0700

    Admin be able to create multiple tables a time

--- src/pages/setup/tables/NewTablePage.js
+++ src/pages/setup/tables/NewTablePage.js
@@ -7,7 +7,8 @@ function NewTablePage(props) {
 
   const [error_message, setErrorMessage] = useState('');
   const [table, setTable] = useState({
-    name: ''
+    name: '',
+    amount: ''
   });
 
   const handleSubmit = (event) => {
@@ -16,12 +17,11 @@ function NewTablePage(props) {
     const newRecord = async () => {
       let endpoint = `/restaurants/${restaurant_id}/tables`;
       await ApiService.apiPost(endpoint, data);
-      setTable({ name: '' });
+      setTable({ name: '', amount: '' });
       props.handleReload();
     }
 
     newRecord().catch(function (error) {
-      console.log('error_message =>', error);
       const error_message = error.response.data.error.message;
       setErrorMessage(error_message);
     });
@@ -33,20 +33,39 @@ function NewTablePage(props) {
   }
 
   return (
-    <div className='mx-4'>
+    <div>
       <form onSubmit={handleSubmit}>
+
+        <div className='text-blue-500 text-sm mb-3'>
+          <p>* ถ้าระบุ <b className='text-blue-600'>จำนวนโต๊ะ</b> จะได้ <b className='text-blue-600'>ชื่อโต๊ะ + ลำดับ</b> เช่น "ชื่อโต๊ะ 1", "ชื่อโต๊ะ 2", ...</p>
+          <p>* ถ้าไม่ระบุ <b className='text-blue-600'>จำนวนโต๊ะ</b> จะได้ชื่อโต๊ะตามที่ระบุ</p>
+        </div>
         <div className='flex'>
-          <div className='w-1/3'>
+          <div className='w-2/6'>
+            <div className='text-gray-500'>
+              * ชื่อโต๊ะ
+            </div>
             <input 
               type="text"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="name"
               name="name"
               value={table.name || ''}
-              placeholder="ชื่อโต๊ะ"
               onChange={handleChange} />
           </div>
-          <div className='w-1/3 ml-3'>
+          <div className='w-1/6 ml-3'>
+            <div className='text-gray-500'>
+              จำนวนโต๊ะ
+            </div>
+            <input 
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="amount"
+              name="amount"
+              value={table.amount}
+              onChange={handleChange} />
+          </div>
+          <div className='w-2/3 ml-3 mt-6'>
             <div className='flex items-center justify-between'>
               <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
                 เพิ่ม
